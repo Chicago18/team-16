59363e2ce5bb4cdc935924ba5a50c1ed478eb0f4
Jonathan Phipps <jonathanmp25@gmail.com>
2018-10-12 21:11:15 -0500

    Create basic ERG component

---
+++ frontend/src/components/ergPage/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ERGPage = () => (
+  <div>
+    <h2>Name</h2>
+    <h3>Location</h3>
+  </div>
+);
--- frontend/src/components/routes/index.js
+++ frontend/src/components/routes/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import ERGPage from '../ergPage';
+
 const Routes = () => (
   <div>
     <Switch>
       <Route exact path='/' component={WelcomePage}/>
+      <Route path='/erg' component={ERGPage}/>
     </Switch>
   </div>
 )
