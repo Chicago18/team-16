b6590d56f0ef3260efe2e8afbf0c81ea1d442797
Jonathan Phipps <jonathanmp25@gmail.com>
2018-10-13 11:22:38 -0500

    remove case sensitivity

--- final/resourcesScript.js
+++ final/resourcesScript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("form").on("submit", function(e){
       e.preventDefault();
-      const query = $($(this).children()[0]).val();
+      const query = $($(this).children()[0]).val().toLowerCase();
 
       $(".list").children().each(function(index) {
         const save = $(this);
@@ -9,12 +9,12 @@ $(document).ready(function(){
         let found = false;
         $(this).find('.tag-cont').children().each(function(index) {
           console.log($(this));
-          if($(this).text() === query) {
+          if($(this).text().toLowerCase() === query) {
             found = true;
           }
         });
         if(!found) {
-          if($(this).find('.resource-name').text() !== query) {
+          if($(this).find('.resource-name').text().toLowerCase() !== query) {
             $(this).hide();
           }
         }
