d2b0345c35c5357d6f2afe7d689ef4ab9cd91bbb
Jonathan Phipps <jonathanmp25@gmail.com>
2018-10-12 19:26:19 -0500

    Add webpack file loaders and css loaders

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,13 +1,34 @@
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
       }
     ]
   },
@@ -24,6 +45,15 @@ module.exports = {
   ],
   devServer: {
     contentBase: './dist',
-    hot: true
+    historyApiFallback: true,
+    hot: true,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    }
   }
 };
