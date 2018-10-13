8b8299cc8d76aaff738bff5308bd344ade0a2f7e
Jonathan Phipps <jonathanmp25@gmail.com>
2018-10-12 21:21:24 -0500

    Add basic blog post component

---
+++ frontend/src/components/ergPage/blogPosts/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BlogPosts = () => (
+  <div>
+
+  </div>
+);
+
+export default BlogPosts;
--- frontend/src/components/ergPage/index.js
+++ frontend/src/components/ergPage/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import BlogPosts from './blogPosts';
+
 const ERGPage = () => (
   <div>
     <h2>Name</h2>
     <h3>Location</h3>
+    <BlogPosts />
   </div>
 );
+
+export default ERGPage;
