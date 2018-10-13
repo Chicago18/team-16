2ce33da5224a4ee3ceb81e00e4210f7456751e0d
lindseyma <lma2@stuy.edu>
2018-10-13 00:40:08 -0500

    figured out the form next isis understanding dispatch ouch

---
+++ frontend/src/components/header/LoginHeader/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+const LoginForm = ({handleSubmit}) => (
+	<div> 
+		<form onSubmit = {handleSubmit}>
+			<Field name = "username" component = "input" type = "text" placeholder = "username" />
+			<Field name = "password" component = "input" type = text" placeholder = "password" />
+			<button name = "login-submit" type = "submit">log in </button>
+			<button name = "register-submit" type = "submit"> register </button>
+		</form>
+	</div>
+)
+
