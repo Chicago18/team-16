e9bc9c585781cdb055aa6aa05a698c5cdb0ae8a3
Jonathan Phipps <jonathanmp25@gmail.com>
2018-10-12 21:39:11 -0500

    Add basic Welcome Page component

--- frontend/src/components/routes/index.js
+++ frontend/src/components/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import WelcomePage from '../welcomePage';
 import ERGPage from '../ergPage';
 
 const Routes = () => (
---
+++ frontend/src/components/welcomePage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const WelcomePage = () => (
+  <div>
+    
+  </div>
+)
+
+export default WelcomePage;
