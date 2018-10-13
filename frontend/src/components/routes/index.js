6a5db72605fd72ece89a9134a69c1ddfa11d4152
Jonathan Phipps <jonathanmp25@gmail.com>
2018-10-12 22:09:34 -0500

    Set up routing

--- frontend/src/components/routes/index.js
+++ frontend/src/components/routes/index.js
@@ -3,12 +3,16 @@ import { Switch, Route } from 'react-router-dom';
 
 import WelcomePage from '../welcomePage';
 import ERGPage from '../ergPage';
+import UserHome from '../userHome';
+import ResourcesPage from '../resourcesPage';
 
 const Routes = () => (
   <div>
     <Switch>
       <Route exact path='/' component={WelcomePage}/>
+      <Route path='/home' component={UserHome}/>
       <Route path='/erg' component={ERGPage}/>
+      <Route path='/resources' component={ResourcesPage}/>
     </Switch>
   </div>
 )
