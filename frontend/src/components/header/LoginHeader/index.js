70726adaabd8388f2ce982a2a78b66ab1f75402d
lindseyma <lma2@stuy.edu>
2018-10-13 01:43:14 -0500

    dispatch

--- frontend/src/components/header/LoginHeader/index.js
+++ frontend/src/components/header/LoginHeader/index.js
@@ -5,10 +5,15 @@ const LoginForm = ({handleSubmit}) => (
 	<div> 
 		<form onSubmit = {handleSubmit}>
 			<Field name = "username" component = "input" type = "text" placeholder = "username" />
-			<Field name = "password" component = "input" type = text" placeholder = "password" />
+			<Field name = "password" component = "input" type = "text" placeholder = "password" />
 			<button name = "login-submit" type = "submit">log in </button>
 			<button name = "register-submit" type = "submit"> register </button>
 		</form>
 	</div>
 )
 
+const mapDispatchToProps = dispatch => ({
+  onLogin: (user) => {
+    dispatch(getUser(user));
+  },
+});
